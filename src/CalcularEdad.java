67c68a8feb01dac9bc57ec131e8a2bca26105ff6
alumno <alumno@gmail.com>
2024-09-23 19:40:03 +0200

    Ej1

---
+++ src/CalcularEdad.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CalcularEdad {
+    public static void main(String[] args) throws Exception {
+        
+
+        Scanner scannerRef = new Scanner(System.in);
+
+        int anioActual, anioNac;
+
+
+        System.out.println("Escriba el año actual: ");
+        anioActual = scannerRef.nextInt();
+
+        System.out.println("Escriba su año de nacimiento: ");
+        anioNac = scannerRef.nextInt();
+
+        int edad = anioActual - anioNac;
+        System.out.printf("Su edad es de %d años.\n", edad);
+
+        scannerRef.close();
+
+
+
+
+        /*
+         
+
+        anioActual = LocalDate.now().getYear();
+        System.out.println("Estamos en: ",+ anioActual);
+        System.out.println("Escriba su año de nacimiento: ");
+        anioNac = scannerRef.nextInt();
+        int edad = anioActual - anioNac;
+        System.out.printf("Su edad es: %d años", edad);
+        
+         */
+
+    }
+}
